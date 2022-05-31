2d183fdc7fd49669417b4723cba68c00fc55108b
Miguel <mwicke@gmail.com>
2022-05-31 14:45:11 +0200

    first try

---
+++ main.js
@@ -0,0 +1,12 @@
+const pass = document.querySelector(".password");
+
+const passwordGenerator = () => {
+  let letters = "0123456789ABCDEF";
+  let randomPass = "";
+  for (let i = 0; i < 6; i++) {
+    randomPass += letters[Math.floor(Math.random() * letters.length)];
+  }
+  //randomPass += "Nice One";
+  pass.innerHTML = randomPass;
+  return randomPass;
+};
