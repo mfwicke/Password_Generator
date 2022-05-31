81c4a8db086e4c1e58b7060a6ce924f1a209020d
Miguel <mwicke@gmail.com>
2022-05-31 15:22:52 +0200

    2th try

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const pass = document.querySelector(".password");
 
 const passwordGenerator = () => {
-  let letters = "0123456789ABCDEF";
+  let letters = "0123456789ABCDEF!@#$%^&*()";
   let randomPass = "";
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 10; i++) {
     randomPass += letters[Math.floor(Math.random() * letters.length)];
   }
   //randomPass += "Nice One";
